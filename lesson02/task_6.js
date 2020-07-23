b89f787e7c40bc549c8bf39b4b0065dd751a5921
Radif <23radif@gmail.com>
2020-07-23 19:17:13 +0300

    Урок 2. Основные операторы JavaScript

--- lesson02/task_6.js
+++ lesson02/task_6.js
@@ -32,7 +32,7 @@ function declensionWord(num, words) {
         if (num % 100 !== 11) {
             return words[0];
         }
-    } else if (num % 10 === 1) {
+    } else if (num % 10 === 2 || num % 10 === 3 || num % 10 === 3) {
         if (num % 100 !== 12 && num % 100 !== 13 && num % 100 !== 14) {
             return words[1]
         }
