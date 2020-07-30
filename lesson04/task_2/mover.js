4a7191381c84acb7302332e809115899260b1668
Radif <23radif@gmail.com>
2020-07-30 16:04:56 +0300

    'lesson04'

--- lesson04/task_2/mover.js
+++ lesson04/task_2/mover.js
@@ -5,18 +5,19 @@ let mover = {
      */
     getDirection() {
         // Доступные значения ввода.
-        const availableDirections = [2, 4, 6, 8];
+        const availableDirections = [2, 4, 6, 8, 1, 3, 7, 9];
 
         while (true) {
             // Получаем от пользователя направление.
-            let direction = parseInt(prompt('Введите число (2, 4, 6 или 8), куда вы хотите переместиться, "Отмена" для выхода.'));
+            let direction = parseInt(prompt('Введите число (1, 2, 3, 4, 6, 7, 8, 9), куда вы хотите переместиться, "Отмена" для выхода.'));
             if (isNaN(direction)) {
                 return null;
             }
             // Если направление не одно из доступных, то сообщаем что надо ввести корректные данные
             // и начинаем новую итерацию.
             if (!availableDirections.includes(direction)) {
-                alert('Для перемещения необходимо ввести одно из чисел 2, 4, 6 или 8.');
+                alert('Для перемещения по горизонтали и вертикали необходимо ввести одно из чисел 2, 4, 6, 8 ' +
+                    '\nДля перемещения по диагонали: 1, 3, 7, 9.');
                 continue;
             }
 
@@ -49,6 +50,22 @@ let mover = {
                 break;
             case 8:
                 nextPosition.y--;
+                break;
+            case 7:
+                nextPosition.y--;
+                nextPosition.x--;
+                break;
+            case 9:
+                nextPosition.y--;
+                nextPosition.x++;
+                break;
+            case 1:
+                nextPosition.y++;
+                nextPosition.x--;
+                break;
+            case 3:
+                nextPosition.y++;
+                nextPosition.x++;
                 break;
         }
 
