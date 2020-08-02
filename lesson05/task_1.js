36c940be7ab37d5ddbbfc2946aea8f96ade2f9a6
Radif <23radif@gmail.com>
2020-08-02 14:30:20 +0300

    Урок 5. Введение в DOM

---
+++ lesson05/task_1.js
@@ -0,0 +1,6 @@
+/*3. a. Сделайте модальное (появляющееся и исчезающее) окно
+b. (не обязательное задание) Сделайте анимацию при появлении и исчезании. Анимации можно взять здесь:
+1. https://github.com/miniMAC/magic (демо https://www.minimamente.com/project/magic/ )
+2. https://github.com/daneden/animate.css (демо https://daneden.github.io/animate.css/ )
+3. http://animista.net/
+*/
