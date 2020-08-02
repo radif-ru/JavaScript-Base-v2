c7eb7b6ae97859b8c3a875f10f198db5fdaab01a
Radif <23radif@gmail.com>
2020-08-02 19:40:57 +0300

    Урок 5. Введение в DOM

---
+++ lesson05/js/task_1.js
@@ -0,0 +1,42 @@
+/*3. a. Сделайте модальное (появляющееся и исчезающее) окно
+b. (не обязательное задание) Сделайте анимацию при появлении и исчезании. Анимации можно взять здесь:
+1. https://github.com/miniMAC/magic (демо https://www.minimamente.com/project/magic/ )
+2. https://github.com/daneden/animate.css (демо https://daneden.github.io/animate.css/ )
+3. http://animista.net/
+*/
+
+let modalWindow = {
+    init() {
+        this.body = document.body;
+        this.container = document.querySelector('.container');
+        this.modal = document.querySelector('.modal');
+        this.button = document.querySelector('.modal-window-button');
+        this.activateEventListener();
+    },
+
+    activateEventListener() {
+        this.button.addEventListener('click', function () {
+            modalWindow.toggleClasses();
+            modalWindow.innerButtonContent();
+        })
+    },
+
+    toggleClasses() {
+        this.container.classList.toggle('modal-window_flex-pos');
+        this.body.classList.toggle('modal-window__bg-gray');
+        this.modal.classList.toggle('modal-window');
+        this.modal.classList.toggle( 'modal-window_flex-pos');
+        this.modal.classList.toggle( 'rotate-hor-center');
+    },
+
+    innerButtonContent() {
+        this.button.innerHTML === 'Открыть модальное окно' ?
+            this.button.innerHTML = 'Закрыть модальное окно' :
+            this.button.innerHTML = 'Открыть модальное окно'
+    }
+
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+    modalWindow.init()
+});
