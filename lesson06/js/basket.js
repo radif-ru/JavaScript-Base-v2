24df8cc2cebf2a4cf24ae07c81a7f26f8af494f9
Radif <23radif@gmail.com>
2020-08-06 23:03:04 +0300

    Урок 6. Обработка событий в JavaScript

--- lesson06/js/basket.js
+++ lesson06/js/basket.js
@@ -1,14 +1,35 @@
-const basket = {
+'use strict';
+
+/**
+ * Объект вешает обработчики событий для корзины и товаров, считает количество выбранных товаров
+ */
+const basketAndGoods = {
     init() {
         this.goodsContainer = document.querySelector('.goods-container');
         this.basket = document.querySelector('.basket');
+        this.basketButton = this.basket.querySelector('.basket-button');
         this.basketGoods = this.basket.querySelector('.basket-goods');
-        this.basketGoodsTitle = this.basketGoods.querySelector('.basket-goods-title');
         this.basketGoodsTotal = this.basketGoods.querySelector('.basket-goods-total');
 
-        this.addEventClickGoodButton()
+        this.addEventClickBasketButton();
+        this.addEventClickGoodButton();
     },
 
+    /**
+     * Обработчик для кнопки корзины. Делает видимой корзину при нажатии или скрывает
+     */
+    addEventClickBasketButton() {
+        this.basketButton.addEventListener('click', () => {
+            this.basketGoods.style.display === 'none' ?
+                this.basketGoods.style.display = 'block' :
+                this.basketGoods.style.display = 'none'
+        });
+    },
+
+    /**
+     * Обработчик вешается на кнопки с товарами, вызывает метод добавления товара в корзину
+     * и метод подсчёта итоговой стоимости товаров
+     */
     addEventClickGoodButton() {
         this.goodsContainer.addEventListener('click', (event) => {
             if (event.target.classList.contains('good-button')) {
@@ -18,6 +39,10 @@ const basket = {
         })
     },
 
+    /**
+     * Добавляет товар в корзину, для кнопки удаления товара вызывает метод, обрабатывающий нажатия
+     * @param event Событие клика на кнопку товара, где в дата атрибутах хранятся id, цена, название товара
+     */
     addGoodInBasket(event) {
         let id = event.target.dataset.id;
         let price = event.target.dataset.price;
@@ -28,13 +53,11 @@ const basket = {
             let good = document.createElement('div');
             good.classList.add('basket-goods-good');
             good.innerHTML += `
-                <div class="basket-goods-good">
-                    <span class="goodId">${id}</span>
-                    <span class="goodName">${name}</span>
-                    <span><span class="goodPrice">${price}</span>р</span>
-                    <span class="goodQuantity">1</span>
-                    <span class="removeButton" data-id="${id}">&#10008;</span>
-                </div>`;
+<span class="goodId">${id}</span>
+<span class="goodName">${name}</span>
+<span><span class="goodPrice">${price}</span>р</span>
+<span class="goodQuantity">1</span>
+<span class="removeButton" data-id="${id}">&#10008;</span>`;
             this.basketGoodsTotal.insertAdjacentElement("beforebegin", good);
             this.addEventClickRemoveButton(id);
         } else {
@@ -44,9 +67,11 @@ const basket = {
         }
     },
 
-    addTotalPrice () {
+    /**
+     * Подсчёт итоговой стоимости, вызывается после добавления или удаления товара
+     */
+    addTotalPrice() {
         let total = 0;
-        let quantity = 0;
         this.basketGoods.querySelectorAll('.basket-goods-good').forEach((good) => {
             total += +good.querySelector('.goodPrice').innerHTML *
                 +good.querySelector('.goodQuantity').innerHTML
@@ -54,6 +79,12 @@ const basket = {
         this.basketGoodsTotal.innerHTML = `<span>Итого: </span><span><span>${total}</span>р</span>`
     },
 
+    /**
+     * Обрабатывает нажатия на иконку удаления товара, по переданному id вешает обработчик на конкретную кнопку
+     * при нажатии уменьшает количество товаров, если однотипных товаров меньше 1, удаляет DOM элемент с этим товром
+     * после удаления запускает метод подсчёта итоговой стоимости товаров
+     * @param id
+     */
     addEventClickRemoveButton(id) {
         this.basketGoods.querySelector(`.removeButton[data-id="${id}"]`).addEventListener('click', (event) => {
             let good = event.target.parentNode;
@@ -70,5 +101,6 @@ const basket = {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-    basket.init()
+    basketAndGoods.init()
 });
+
